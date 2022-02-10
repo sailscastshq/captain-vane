08fd61a32ce102c1434939453799b75f0ca60a54
Kelvin Omereshone <kelvinomereshone@gmail.com>
2022-02-10 13:15:36 +0100

    feat: add sails.helpers.vane

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+module.exports = function(sails) {
+  return {
+    initialize: function(done) {
+      if (!sails.hooks.helpers) {
+        return done(new Error('Cannot load captain-vane hook without enabling the "helpers" hook!'));
+      }
+
+      sails.after('hook:helpers:loaded', function() {
+        try {
+          if(!sails.hooks.helpers['vane']) {
+            sails.hooks.helpers.furnishHelper('vane', require('./vane'));
+            return done();
+          }
+        } catch (error) {
+          return done(error);
+        }
+        return done();
+      })
+    }
+  }
+}
+
 const fake = require('@ngneat/falso')
-const hook = require('./hook')
-const generator = require('./generator')
-
-module.exports = hook
-module.exports = generator
-
 module.exports.fake = fake
