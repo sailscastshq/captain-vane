cf75bef7d8e84dcc51139c776069ec9ba4b4072a
Kelvin Omereshone <kelvinomereshone@gmail.com>
2022-02-10 14:30:00 +0100

    fix: sails should see the hook object

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
 const path = require('path')
-module.exports = function(sails) {
-  return {
-    initialize: function(done) {
-      if (!sails.hooks.helpers) {
-        return done(new Error('Cannot load captain-vane hook without enabling the "helpers" hook!'));
-      }
-
-      sails.after('hook:helpers:loaded', function() {
-        try {
-          if(!sails.hooks.helpers['vane']) {
-            sails.hooks.helpers.furnishHelper('vane', require('./vane'));
-            return done();
-          }
-        } catch (error) {
-          return done(error);
-        }
-        return done();
-      })
-    }
-  }
-}
-
 /**
  * Generates new factory file
  */
@@ -55,6 +33,29 @@ module.exports = {
     './config/factories/:relPath': { template: 'factory.template'}
   },
   templatesDirectory: path.resolve(__dirname,'./templates'),
+}
+
+
+module.exports = function(sails) {
+  return {
+    initialize: function(done) {
+      if (!sails.hooks.helpers) {
+        return done(new Error('Cannot load captain-vane hook without enabling the "helpers" hook!'));
+      }
+
+      sails.after('hook:helpers:loaded', function() {
+        try {
+          if(!sails.hooks.helpers['vane']) {
+            sails.hooks.helpers.furnishHelper('vane', require('./vane'));
+            return done();
+          }
+        } catch (error) {
+          return done(error);
+        }
+        return done();
+      })
+    }
+  }
 }
 
 const fake = require('@ngneat/falso')
